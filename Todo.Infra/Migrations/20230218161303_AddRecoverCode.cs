b57a66a3b8b7bc4f5c9b66af827a87b148033c22
fmchtt <felipe@fmchtt.dev.br>
2023-02-18 13:14:08 -0300

    Add recover code migration

---
+++ Todo.Infra/Migrations/20230218161303_AddRecoverCode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRecoverCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RecoverCodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecoverCodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecoverCodes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecoverCodes_UserId",
+                table: "RecoverCodes",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecoverCodes_UserId_Code",
+                table: "RecoverCodes",
+                columns: new[] { "UserId", "Code" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecoverCodes");
+        }
+    }
+}
