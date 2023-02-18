b47990819ce9a8ecf7096114e44dfb3ff894b85a
fmchtt <felipe@fmchtt.dev.br>
2023-02-18 10:42:37 -0300

    Fix User instance to EF

--- Todo.Domain/Entities/RecoverCode.cs
+++ Todo.Domain/Entities/RecoverCode.cs
@@ -3,7 +3,7 @@
 public class RecoverCode : Entity
 {
     public Guid UserId { get; set; }
-    public User User { get; set; }
+    public virtual User User { get; set; }
     public int Code { get; set; }
 
     #pragma warning disable CS8618
