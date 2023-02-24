27754e4f4f6f7bd9611e6bc819c5657ef98fa7dd
fmchtt <felipe@fmchtt.dev.br>
2023-02-24 20:31:42 -0300

    Added invite useCases and model

--- Todo.Domain/Entities/Invite.cs
+++ Todo.Domain/Entities/Invite.cs
@@ -4,9 +4,10 @@ public class Invite : Entity
 {
     public string Email { get; set; }
     public Guid BoardId { get; set; }
-    public Board Board { get; set; }
+    public virtual Board Board { get; set; }
     public DateTime CreatedDate { get; set; } = DateTime.Now;
 
+    #pragma warning disable CS8618
     public Invite(string email, Guid boardId)
     {
         Email = email;
---
+++ Todo.Infra/Migrations/20230224232552_AddInvite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invites",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    BoardId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invites_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invites_BoardId_Email",
+                table: "Invites",
+                columns: new[] { "BoardId", "Email" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invites");
+        }
+    }
+}
