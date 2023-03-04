fef2781641ecefcf09184e63aceafdb053e7729b
fmchtt <felipe@fmchtt.dev.br>
2023-03-03 22:01:52 -0300

    Add file to repository

---
+++ Todo.Domain/Commands/ItemCommands/MarkCommand.cs
@@ -0,0 +1,20 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands.ItemCommands;
+
+public class MarkCommand : ICommand
+{
+    public Guid ItemId { get; set; }
+    public bool Done { get; set; }
+    
+    public MarkCommand(Guid itemId, bool done)
+    {
+        ItemId = itemId;
+        Done = done;
+    }
+    
+    public bool Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
